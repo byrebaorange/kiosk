9b52191b3ade1c0316efcf1271645712f08d8c01
byrebaorange <o2694237@gmail.com>
2026-06-30 19:48:16 +0300

    Начал работать над проектом, сделал главное меню

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Hello world!")
-
-print("Ole ole ole")
+balance = 1000
+inventory = {"хлеб":10}
+while True :
+	print("Баланс",balance)
+	print("Инвентарь",inventory)
+	x = int(input("\nГЛАВНОЕ МЕНЮ\nЧто вы хотите сделать\n1.Посмотреть товары\n2.Дождаться покупателя\n3.Открыть сундук\n4.Выйти из игры\n"))
+	if x == 4:
+		break
+	if x == 1:
+		for i in inventory:
+			print(f"\n{i}: {inventory[i]}")
